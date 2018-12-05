996104549d2f3070b2b7c65734bf2bc57c8580fc
Nickolay Pelov <nikolaypelov@gmail.com>
2018-12-05 14:40:59 +0100

    Added global background decorator to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import '../src/styles.css';
 import '../src/components/components.css';
+import { withBackgrounds } from '@storybook/addon-backgrounds';
+
+addDecorator(
+	withBackgrounds([
+		{ name: 'default', value: '#ffffff', default: true },
+		{ name: 'twitter', value: '#00aced' },
+		{ name: 'facebook', value: '#3b5998' },
+		{ name: 'prussian blue', value: '#0b3c5d' },
+		{ name: 'ivory black', value: '#1d2731' },
+	])
+);
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/components', true, /.stories.js$/);
 function loadStories() {
