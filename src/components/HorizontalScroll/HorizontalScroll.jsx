dfe705dc020f34e9438a63f8499520d9ccac2ade
Nickolay Pelov <nikolaypelov@gmail.com>
2018-12-30 02:32:24 +0100

    Added horizontal scroll view component

---
+++ src/components/HorizontalScroll/HorizontalScroll.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const HorizontalScroll = (props) => {
+	const { children, className, style } = props;
+	let scrollPosition = 0;
+
+	function onWheel(e) {
+		const element = e.currentTarget;
+		e.preventDefault();
+		if (e.deltaY < 0) {
+			// scrolling up
+			scrollPosition -= 40;
+		} else if (e.deltaY > 0) {
+			//scrolling down
+			scrollPosition += 40;
+		}
+		if (scrollPosition < 0) {
+			scrollPosition = 0;
+			return;
+		} else if (scrollPosition > element.scrollWidth) {
+			scrollPosition = element.scrollWidth;
+			return;
+		}
+
+		element.scrollLeft = scrollPosition;
+	}
+
+	return (
+		<div className={classNames('horizontal-scroll', className)} style={style} onWheel={onWheel.bind(this)}>
+			{children}
+		</div>
+	);
+};
+export default HorizontalScroll;
---
+++ src/components/HorizontalScroll/HorizontalScroll.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import HorizontalScroll from './HorizontalScroll';
+import './HorizontalScroll.css';
+import Card from '../Card/Card';
+import '../Card/Card.css';
+import Button from '../Button/Button';
+import '../Button/Button.css';
+import Badge from '../Badge/Badge';
+import '../Badge/Badge.css';
+
+const styles = {
+	card: {
+		width: '400px',
+		height: '350px',
+		backgroundColor: 'red',
+		border: 'solid black',
+	},
+};
+
+storiesOf('HorizontalScroll', module).add('Default', () => (
+	<HorizontalScroll>
+		<div style={styles.card} />
+		<div style={styles.card} />
+		<div style={styles.card} />
+		<div style={styles.card} />
+		<div style={styles.card} />
+	</HorizontalScroll>
+));
