a8b3c9bfa737a18905569ed4cd5566e66b663f93
Nickolay Pelov <nikolaypelov@gmail.com>
2019-01-07 12:38:03 +0100

    Created Avatar Component

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Icon from '../Icon/Icon';
+
+const Avatar = (props) => {
+	const { image, onClick, style, className, size, status } = props;
+	function renderSize(type) {
+		let styles = '';
+
+		switch (size) {
+			case 'L':
+				styles = type === 'image' ? 'avatar-l' : 'avatar-status-l';
+				break;
+			case 'XL':
+				styles = type === 'image' ? 'avatar-xl' : 'avatar-status-xl';
+				break;
+			case 'XXL':
+				styles = type === 'image' ? 'avatar-xxl' : 'avatar-status-xxl';
+				break;
+			case 'XXXL':
+				styles = type === 'image' ? 'avatar-xxxl' : 'avatar-status-xxxl';
+				break;
+			default:
+				styles = type === 'image' ? 'avatar-xxl' : 'avatar-status-xxl';
+				break;
+		}
+
+		return styles;
+	}
+
+	return (
+		<div className={classNames('avatar-container', className)} onClick={onClick}>
+			{image ? (
+				<img src={image} className={classNames('avatar', renderSize('image'))} aria-hidden="true" style={style} />
+			) : (
+				<Icon icon={'icon-account_circle'} size={size ? size : 'XXL'} style={style} />
+			)}
+
+			<div className={classNames('avatar-status', renderSize(), `${status}`)} />
+		</div>
+	);
+};
+
+Avatar.propTypes = {
+	onClick: PropTypes.func,
+	size: PropTypes.oneOfType([PropTypes.oneOf(['L', 'XL', 'XXL', 'XXXL']), PropTypes.number]),
+	status: PropTypes.oneOf(['active', 'offline', 'away', 'busy']),
+};
+export default Avatar;
---
+++ src/components/Avatar/Avatar.stories.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import centered from '@storybook/addon-centered';
+import Avatar from './Avatar';
+import './Avatar.css';
+
+storiesOf('Avatar', module)
+	.addDecorator(centered)
+	.add('Default', () => (
+		<div style={{ display: 'flex', alignItems: 'center' }}>
+			<Avatar />
+			User Name
+		</div>
+	))
+	.add('With Image', () => (
+		<div style={{ display: 'flex', alignItems: 'center' }}>
+			<Avatar
+				image={
+					'https://m.media-amazon.com/images/M/MV5BMjM2OTkyNTY3N15BMl5BanBnXkFtZTgwNzgzNDc2NjE@._V1_CR132,0,761,428_AL_UY268_CR82,0,477,268_AL_.jpg'
+				}
+				size={'XXXL'}
+				status={'offline'}
+			/>
+			Offline
+		</div>
+	))
+	.add('Size', () => (
+		<div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'L'} status={'active'} />
+				Large
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XL'} status={'active'} />
+				Extra Large
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXL'} status={'active'} />
+				Extra Extra Large
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXXL'} status={'active'} />
+				Extra Extra Extra Large
+			</div>
+		</div>
+	))
+	.add('With Status', () => (
+		<div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXXL'} status={'offline'} />
+				Offline
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXXL'} status={'active'} />
+				Active
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXXL'} status={'away'} />
+				Away
+			</div>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Avatar size={'XXXL'} status={'busy'} />
+				Busy
+			</div>
+		</div>
+	));
