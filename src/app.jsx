9bb7fec55e4eec1849c48eef130cdef9325a57c8
Nikolay Pelov <nikolai_pelov@abv.bg>
2019-01-21 13:08:55 +0100

    Added new icons and updated compilerrc file

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import AppRouter from './views/Router';
 import AuthRouter from './views/Auth/AuthRouter';
+import { MenuContext, MenuContextComponent } from './contexts/MenuContext';
 export default class App extends React.Component {
+	static contextType = MenuContext;
 	state = {
 		loggedIn: true,
 	};
+
 	render() {
 		const { loggedIn } = this.state;
-		return loggedIn ? <AppRouter /> : <AuthRouter />;
+		return <MenuContextComponent>{loggedIn ? <AppRouter /> : <AuthRouter />}</MenuContextComponent>;
 	}
 }
